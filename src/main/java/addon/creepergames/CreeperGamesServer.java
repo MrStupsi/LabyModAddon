b7573e2d2ed4679084ee49b6ef41b7b62ff7c29a
Fabian Menth <fabian.menth@gmx.de>
2020-07-19 13:24:33 +0200

    Anfang

---
+++ src/main/java/addon/creepergames/CreeperGamesServer.java
@@ -0,0 +1,66 @@
+package addon.creepergames;
+
+import java.util.List;
+
+import net.labymod.api.events.TabListEvent.Type;
+import net.labymod.servermanager.ChatDisplayAction;
+import net.labymod.servermanager.Server;
+import net.labymod.settings.elements.SettingsElement;
+import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.network.PacketBuffer;
+
+public class CreeperGamesServer extends Server {
+	public CreeperGamesServer() {
+		super("CreeperGames", "griefergames.net");
+	}
+	
+	@Override
+	public void fillSubSettings(List<SettingsElement> list) {
+		
+	}
+	
+	@Override
+	public ChatDisplayAction handleChatMessage(String unformatted, String formatted) throws Exception {
+		if (Main.getAddon().getConfig().get("zweiterchat").getAsBoolean()) {
+			if (unformatted.startsWith("[mir -> ")) {
+				return ChatDisplayAction.SWAP;
+			}
+			if (unformatted.contains("-> mir]")) {
+				return ChatDisplayAction.SWAP;
+			}
+			if (unformatted.startsWith("[Plot-Chat]")) {
+				return ChatDisplayAction.SWAP;
+			}
+			if (unformatted.startsWith("[Spectator-Chat]")) {
+				return ChatDisplayAction.SWAP;
+			}
+			if (unformatted.startsWith("[Global]")) {
+				return ChatDisplayAction.SWAP;
+			}
+			if (unformatted.endsWith(": gg") || unformatted.endsWith(": Gg") ||
+					unformatted.endsWith(": gG") || unformatted.endsWith(": GG")) {
+				return ChatDisplayAction.SWAP;
+			}
+			if (unformatted.endsWith(": bg") || unformatted.endsWith(": Bg") ||
+					unformatted.endsWith(": bG") || unformatted.endsWith(": BG")) {
+				return ChatDisplayAction.SWAP;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public void handlePluginMessage(String arg0, PacketBuffer arg1) throws Exception {
+		
+	}
+	
+	@Override
+	public void handleTabInfoMessage(Type arg0, String arg1, String arg2) throws Exception {
+		
+	}
+	
+	@Override
+	public void onJoin(ServerData data) {
+		
+	}
+}
---
+++ src/main/java/addon/creepergames/Main.java
@@ -0,0 +1,59 @@
+package addon.creepergames;
+
+import java.util.List;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.BooleanElement;
+import net.labymod.settings.elements.ControlElement;
+import net.labymod.settings.elements.SettingsElement;
+import net.labymod.utils.Consumer;
+import net.minecraftforge.client.event.GuiScreenEvent.KeyboardInputEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
+
+public class Main extends LabyModAddon {
+	private static LabyModAddon addon;
+	
+	@Override
+	public void onEnable() {
+		addon = this;
+		getApi().registerServerSupport(this, new CreeperGamesServer());
+		getApi().registerForgeListener(this);
+	}
+	
+	@Override
+	public void loadConfig() {
+		if (!getConfig().has("zweiterchat")) {getConfig().addProperty("zweiterchat", true);}
+		saveConfig();
+	}
+	
+	@Override
+	protected void fillSettings(List<SettingsElement> list) {
+		BooleanElement zweiterChat = new BooleanElement(
+				"Private Nachrichten 2. Chat",
+				new ControlElement.IconData("labymod/textures/settings/settings/chatpositionright.png"),
+				new Consumer<Boolean>() {
+					public void accept(Boolean zweiterchat) {
+						getConfig().remove("zweiterchat");
+						getConfig().addProperty("zweiterchat", zweiterchat);
+						saveConfig();
+					}
+				},
+				true);
+	    list.add(zweiterChat);
+	}
+	
+	public static LabyModAddon getAddon() {
+		return addon;
+	}
+	
+	@SubscribeEvent
+	public void onTick(ClientTickEvent e) {
+		
+	}
+	
+	@SubscribeEvent
+	public void onKey(KeyboardInputEvent e) {
+		
+	}
+}
